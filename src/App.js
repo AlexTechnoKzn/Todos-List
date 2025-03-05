a1ef7f4936571e8c5800450600a9f0525b9e56ef
Alex <noclafsake50@gmail.com>
2025-03-06 01:33:25 +0300

    point 1 completed

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import { Todo } from './components';
+import styles from './App.module.css';
+
+export const App = () => {
+	const [todos, setTodos] = useState([]);
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((todosData) => todosData.json())
+			.then((loadedTodos) => setTodos(loadedTodos));
+	}, []);
+
+	return (
+		<div className={styles.app}>
+			<div>
+				{todos.map(({ id, title, completed }) => (
+					<Todo key={id} title={title} completed={completed} />
+				))}
+			</div>
+		</div>
+	);
+};
